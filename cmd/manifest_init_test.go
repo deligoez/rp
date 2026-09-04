964dff61b230acb7e8db273bd5850b2a4866cfba
Yunus Emre Deligöz <ye@deligoz.me>
2026-09-04 13:51:52 +0300

    test: cover GitHub remote URL parsing

---
+++ cmd/manifest_init_test.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestParseGitHubRemote(t *testing.T) {
+	cases := []struct {
+		name        string
+		url         string
+		owner, repo string
+	}{
+		{"ssh with .git", "git@github.com:deligoez/rp.git", "deligoez", "rp"},
+		{"ssh without .git", "git@github.com:deligoez/rp", "deligoez", "rp"},
+		{"https with .git", "https://github.com/deligoez/rp.git", "deligoez", "rp"},
+		{"https without .git", "https://github.com/deligoez/rp", "deligoez", "rp"},
+		{"surrounding whitespace", "  git@github.com:deligoez/rp.git\n", "deligoez", "rp"},
+		{"name containing dots", "git@github.com:deligoez/my.tool.git", "deligoez", "my.tool"},
+		{"name ending in git", "git@github.com:deligoez/gogit.git", "deligoez", "gogit"},
+
+		{"other host over ssh", "git@gitlab.com:deligoez/rp.git", "", ""},
+		{"other host over https", "https://gitlab.com/deligoez/rp.git", "", ""},
+		{"http is not https", "http://github.com/deligoez/rp", "", ""},
+		{"extra path segment", "https://github.com/deligoez/rp/extra", "", ""},
+		{"missing repo", "git@github.com:deligoez", "", ""},
+		{"empty", "", "", ""},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			owner, repo := parseGitHubRemote(tc.url)
+			if owner != tc.owner || repo != tc.repo {
+				t.Errorf("parseGitHubRemote(%q) = (%q, %q), want (%q, %q)", tc.url, owner, repo, tc.owner, tc.repo)
+			}
+		})
+	}
+}
+
